7b1ce93d3b7145dba80ac87fcdf7ad2a75bd41b7
Jim Thorson <JamesT.esq@gmail.com>
2016-04-06 13:30:01 -0700

    added guidance for Week 1 homework

---
+++ Week 1 -- Likelihoods and linear models/Homework/Homework.R
@@ -0,0 +1,7 @@
+
+setwd( "C:/Users/James.Thorson/Desktop/Project_git/2016_Spatio-temporal_models/Week 1 -- Likelihoods and linear models/Homework/" )
+
+library( TMB )
+
+compile( "homework_delta_model.cpp" )
+
