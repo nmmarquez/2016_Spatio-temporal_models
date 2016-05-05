ea8a4d9eb45c543b75155847252256c1a02a2a65
nmmarquez <nmmarquez@gmail.com>
2016-05-04 23:10:18 -0700

    hw 5 code

--- Week 5 -- 1D spatial models/Homework/hw5.R
+++ Week 5 -- 1D spatial models/Homework/hw5.R
@@ -5,7 +5,7 @@ library(knitr)
 library(RandomFields)
 setwd("~/Documents/Classes/2016_Spatio-temporal_models/Week 5 -- 1D spatial models/Homework/")
 
-model_name <- "hw3"
+model_name <- "hw5"
 if (file.exists(paste0(model_name, ".so"))) file.remove(paste0(model_name, ".so"))
 if (file.exists(paste0(model_name, ".o"))) file.remove(paste0(model_name, ".o"))
 if (file.exists(paste0(model_name, ".dll"))) file.remove(paste0(model_name, ".dll"))
@@ -32,9 +32,34 @@ Sim_Fn <- function(n_i=1000, Scale=2, logSD_spatial=0.1, L0=10, Linf_0=100,
     
     # Bundle and return stuff
     DF <- data.frame( "y_i"=y_i, "a_i"=a_i, "l_i"=l_i, "Linf_i"=Linf_i)
+    DF <- DF[order(DF$y_i),]
     DF
 }
 
 df <- Sim_Fn( n_i=1000 )
+use_site <- TRUE
 
-summary(df)
+run_model <- function(df, use_site=FALSE){
+    N <- nrow(df)
+    covs <- use_site + 1
+    X <- df[, c("Linf_i", "y_i")]; X[,1] <- 1
+    random <- c("epsilon", "pi")
+    dyn.load(dynlib(model_name))
+    Map <- list(log_sigma_e=factor(NA), epsilon=factor(rep(NA, N)))
+    Map <- list(logit_rho=factor(NA), 
+                log_sigma_p2=factor(NA), pi=factor(rep(NA, N)))
+    Params <- list(pi=rep(0, N), epsilon=rep(0, N), beta=rep(0, covs), log_l0=0,
+                   logit_rho=0, log_sigma_e=0, log_sigma_p2=0, log_sigma_l=0,
+                   log_k=0)
+    Data <- list(Y=df$l_i, A=df$a_i, P=df$y_i, X=as.matrix(X[,1:covs]))
+    Obj <- MakeADFun(data=Data, parameters=Params, DLL=model_name, random=random, map=Map)
+    Opt <- nlminb(start=Obj$par, objective=Obj$fn, gradient=Obj$gr)
+    Report <- Obj$report()
+    SD <- sdreport(Obj)
+    dyn.unload(dynlib(model_name))
+    params <- c("b", "sigma_proc", "sigma_obs")
+    if (use_alpha){
+        params <- c("a", "b", "sigma_proc", "sigma_obs") 
+    }
+    summary(SD)[params, 1]
+}
--- Week 5 -- 1D spatial models/Homework/hw5.cpp
+++ Week 5 -- 1D spatial models/Homework/hw5.cpp
@@ -15,22 +15,77 @@ Type objective_function<Type>::operator() () {
     DATA_VECTOR(Y);
     DATA_VECTOR(A);
     DATA_VECTOR(P);
-    
-    PARAMETER_VECTOR(l_inf);
-    PARAMETER(l0);
-    PARAMETER(k);
-    PARAMETER(log_sigma);
-    
-    int N y.size();
-    
-    yhat vector<Type> (N);
-    for(int i = 0; i < n; i++){
-        if (l_inf.size() == 1){
-            yhat[i] = exp(l_inf[0] - (l_inf[0] - l0) * exp(-k * A[i]));
-        }
-        else{
-            yhat[i] = exp(l_inf[i] - (l_inf[i] - l0) * exp(-k * A[i]));
+    DATA_MATRIX(X);
+
+    PARAMETER(logit_rho);
+    PARAMETER_VECTOR(beta);
+    PARAMETER_VECTOR(pi);
+    PARAMETER_VECTOR(epsilon);
+    PARAMETER(log_l0);
+    PARAMETER(log_k);
+    PARAMETER(log_sigma_e);
+    PARAMETER(log_sigma_p2);
+    PARAMETER(log_sigma_l);
+
+    // Bookkeeping variables
+    int N = Y.size();
+    int K = X.cols();
+
+    // exponentiate variables
+    Type l0 = exp(log_l0);
+    Type k = exp(log_k);
+    Type sigma_p2 = exp(log_sigma_p2);
+    Type sigma_e = exp(log_sigma_e);
+    Type sigma_l = exp(log_sigma_l);
+    Type rho = 1 / (1 + exp(-logit_rho));
+
+    vector<Type> log_l_inf(N);
+    vector<Type> l_inf(N);
+    vector<Type> l_inf_obs(N);
+    vector<Type> y_pred(N);
+
+    // linear prediction
+    for(int n = 0; n < N; n++){
+        log_l_inf[n] = pi[n] + epsilon[n];
+        for(int k = 0; k < K; k++){
+            log_l_inf[n] += beta[k] * X(n,k);
         }
     }
-    
+
+    l_inf = exp(log_l_inf);
+
+    for(int n = 0; n < N; n++){
+        y_pred[n] = l_inf[n] - ((l_inf[n] - l0) * exp(Type(-1.) * k * A[n]));
+    }
+
+    // record nll
+    Type nll = 0.;
+
+    // random effects structure
+    Type dist;
+    for(int n = 0; n < N; n++){
+        // nll -= dnorm(epsilon[n], Type(0.), sigma_e);
+        if(n == 0){
+            nll -= dnorm(pi[n], Type(0.), pow(sigma_p2, .5));
+        }
+        else{
+            dist = P[n]-P[n-1];
+            nll -= dnorm(pi[n], pow(rho,dist)*pi[n-1], pow(sigma_p2*(1-pow(rho,2*dist)), 0.5), true);
+        }
+    }
+
+    // data likelihood
+    for(int n = 0; n < N; n++){
+        nll -= dnorm(log(y_pred[n]), log(Y[n]), sigma_l, true);
+    }
+
+    // report the parameters I need
+    REPORT(y_pred);
+    REPORT(l_inf);
+    REPORT(epsilon);
+    REPORT(sigma_l);
+    REPORT(sigma_p2);
+    REPORT(rho);
+
+    return nll;
 }
