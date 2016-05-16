259cf179cdd2e10f91cef482675c24b691206c70
nmmarquez <nmmarquez@gmail.com>
2016-05-16 14:18:55 -0700

    3 random effects seperable structure

--- mort_project/gpz.cpp
+++ mort_project/gpz.cpp
@@ -13,19 +13,24 @@ Type objective_function<Type>::operator() ()
     printf("%s\n", "Data loaded");
     
     // Parameters
-    PARAMETER_VECTOR(log_gpz);
+    PARAMETER_VECTOR(gpz);
     PARAMETER(log_sigma_obs);
     PARAMETER_VECTOR(epsilon_age);
     PARAMETER_VECTOR(epsilon_time);
+    PARAMETER_ARRAY(epsilon_age_time);
     PARAMETER(logit_rho_age);
     PARAMETER(logit_rho_time);
+    PARAMETER(logit_rho_age2);
+    PARAMETER(logit_rho_time2);
     printf("%s\n", "Parameters loaded");
     
     // Transforms
-    vector <Type> gpz = exp(log_gpz);
+    // vector <Type> gpz = exp(log_gpz);
     Type sigma_obs = exp(log_sigma_obs);
     Type rho_age = 1 / (1 + exp(-logit_rho_age));
     Type rho_time = 1 / (1 + exp(-logit_rho_time));
+    Type rho_age2 = 1 / (1 + exp(-logit_rho_age2));
+    Type rho_time2 = 1 / (1 + exp(-logit_rho_time2));
     int N = log_rate_mort.size();
     printf("%s\n", "Data Transformed");
     
@@ -35,6 +40,7 @@ Type objective_function<Type>::operator() ()
     // Probability of random effects
     vector<Type> age_effect(N);
     vector<Type> time_effect(N);
+    vector<Type> age_time_effect(N);
 
     using namespace density;
     if (option >= 2){
@@ -44,21 +50,25 @@ Type objective_function<Type>::operator() ()
     if (option >= 3){
         nll += AR1(rho_time)(epsilon_time);
     }
+    if (option >= 4){
+        nll += SEPARABLE(AR1(rho_age2), AR1(rho_time2))(epsilon_age_time);
+    }
 
     printf("%s\n", "Setting age effects per observation");
     for(int n=0; n<N; n++){
         age_effect[n] = epsilon_age[age_group[n]];
         time_effect[n] = epsilon_time[time_group[n]];
+        age_time_effect[n] = epsilon_age_time(age_group[n], time_group[n]);
     }
     
     printf("%s\n", "make_predictions");
-    Type alpha = gpz[0];
+    Type alpha = exp(gpz[0]);
     Type beta = gpz[1];
-    Type gamma = gpz[2];
-    Type delta = gpz[3];
+    Type gamma = exp(gpz[2]);
+    Type delta = exp(gpz[3]);
     Type zeta = gpz[4];
-    log_rate_mort_hat = log(alpha * exp(-1. * beta * age) + gamma +
-        delta * exp(zeta * age)) + age_effect + time_effect;
+    vector<Type> log_rate_mort_hat = log(alpha * exp(-1. * beta * age) + gamma +
+        delta * exp(zeta * age)) + age_effect + time_effect + age_time_effect;
 
     printf("%s\n", "evaluate data likelihood");
     for(int n=0; n<N; n++){
--- mort_project/simulate_data.R
+++ mort_project/simulate_data.R
@@ -1,5 +1,6 @@
 rm(list=ls())
 library(TMB)
+library(INLA)
 setwd("~/Documents/Classes/2016_Spatio-temporal_models/mort_project/")
 source('./mort_viz/db_access.R')
 source("./mort_viz/utilities.R")
@@ -26,40 +27,45 @@ df$time_group <- df$year_id - min(df$year_id)
 df <- subset(df, sex_id == 1)
 
 run_model <- function(df, option=1, model_name=model, print=F){
-    N <- nrow(df)
+    N_ <- nrow(df)
+    T_ <- length(unique(df$time_group))
+    A_ <- length(unique(df$age_group))
     Map <- list()
-    Random <- c("epsilon_age", "epsilon_time")
-    if (option == 0){
+    Random <- c("epsilon_age", "epsilon_time", "epsilon_age_time")
+    if (option <= 0){
         Map[["epsilon_age"]] <- factor(rep(NA, length(unique(df$age_group))))
-        Map[["epsilon_time"]] <- factor(rep(NA, length(unique(df$time_group))))
-        Map[["logit_rho_age"]] <- factor(NA)
-        Map[["logit_rho_time"]] <- factor(NA)
         Random <- NULL
     }
-    if (option == 1){
-        Map[["epsilon_time"]] <- factor(rep(NA, length(unique(df$time_group))))
+    if (option <= 1){
         Map[["logit_rho_age"]] <- factor(NA)
-        Map[["logit_rho_time"]] <- factor(NA)
         Random <- NULL
     }
-    if (option == 2){
+    if (option <= 2){
         Map[["epsilon_time"]] <- factor(rep(NA, length(unique(df$time_group))))
         Map[["logit_rho_time"]] <- factor(NA)
         Random <- c("epsilon_age")
     }
+    if (option <= 3){
+        Map[["epsilon_age_time"]] <- factor(matrix(NA, nrow=A_, ncol=T_))
+        dim(Map[["epsilon_age_time"]]) <- c(A_, T_)
+        Map[["logit_rho_age2"]] <- factor(NA)
+        Map[["logit_rho_time2"]] <- factor(NA)
+        Random <- c("epsilon_age", "epsilon_time")
+    }
     dyn.load(dynlib(model_name))
-    Params <- list(log_gpz=rep(0, 5), log_sigma_obs=0, logit_rho_time=0,
+    Params <- list(gpz=rep(0, 5), log_sigma_obs=0, logit_rho_time=0,
                    epsilon_age=rep(0, length(unique(df$age_group))),
                    epsilon_time=rep(0, length(unique(df$time_group))),
-                   logit_rho_age=0)
+                   logit_rho_age=0, logit_rho_age2=0, logit_rho_time2=0,
+                   epsilon_age_time=matrix(0, nrow=A_, T_))
     Data <- list(log_rate_mort=df$log_rate, age=df$age_mean, option=option,
                  age_group=df$age_group, time_group=df$time_group)
     Obj <- MakeADFun(data=Data, parameters=Params, DLL=model_name,
                      silent=!print, map=Map, random = Random)
     Obj$env$tracemgc <- print
     Obj$env$inner.control$trace <- print
-    Opt <- nlminb(start=Obj$par, objective=Obj$fn, gradient=Obj$gr,)
-                  #control=list(iter.max=1000, eval.max=500))
+    Opt <- nlminb(start=Obj$par, objective=Obj$fn, gradient=Obj$gr,
+                  control=list(iter.max=1000, eval.max=500))
     Report <- Obj$report()
     dyn.unload(dynlib(model_name))
     if(Opt$convergence != 0){
@@ -71,11 +77,11 @@ run_model <- function(df, option=1, model_name=model, print=F){
 #gpzm_sub <- run_model(subset(df, age_mean > 25), option=0)
 #gpzm <- run_model(df, option=0)
 #silder <- run_model(subset(df, age_mean < 80), option=1)
-silder <- run_model(df, option=3, print=T)
+silder <- run_model(df, option=4, print=T)
 
 test_ages <- sort(unique(df$age_mean))
 fitted_vals <- sim_data(test_ages, a=silder$alpha, b=silder$beta,
-                        c=silder$gamma, d=silder$delta, f=silder$zeta)
+                         c=silder$gamma, d=silder$delta, f=silder$zeta)
 fitted_df <- data.frame(test_ages, log(fitted_vals), year_id=2020)
 time_plot(df, 1) + geom_line(aes(test_ages, log.fitted_vals.), fitted_df)
 fitted_df$log.fitted_vals. <- fitted_df$log.fitted_vals. + silder$epsilon_age
